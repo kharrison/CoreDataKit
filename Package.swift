8f2a32d6115e2adc57cad09c9449329bf13e9e70
Keith Harrison <keith@useyourloaf.com>
2021-11-25 15:07:39 +0000

    Update the package supported platform. - Xcode 13 was failing to build on macOS, tvOS and watchOS - Update to iOS 13 equivalent versions

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "CoreDataKit",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         .library(
